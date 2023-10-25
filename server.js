d14da577a7f9e33d4bb775f0019df13a08ee112b
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-10-25 16:47:41 -0300

    03-Connecting to DB
    03-Connecting to DB

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
+// francis
+// HSVKmzgrkIxiOEMu
+
+const mongoURI = `mongodb+srv://francis:HSVKmzgrkIxiOEMu@cluster0.soln6fi.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(mongoURI,{
+    serverApi:{
+        version:ServerApiVersion.v1,
+        strict:true,
+        deprecationErrors:true
+    }
+});
+
+async function run(){
+    try{
+        await client.connect();
+        console.log('connected')
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir)
 
 const port = process.env.PORT || 3001;
 app.listen(port);
