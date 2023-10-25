67294fef4205d6d6257ea4696dc71dfa7626d399
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-10-25 17:17:50 -0300

    05-Mongoose
    05-Mongoose

--- server.js
+++ server.js
@@ -1,51 +1,24 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 
 // francis
 // HSVKmzgrkIxiOEMu
 
 const mongoURI = `mongodb+srv://francis:HSVKmzgrkIxiOEMu@cluster0.soln6fi.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(mongoURI);
 
-const client = new MongoClient(mongoURI,{
-    serverApi:{
-        version:ServerApiVersion.v1,
-        strict:true,
-        deprecationErrors:true
-    }
+
+///////// SCHEMA  //////////////
+const carSchema = mongoose.Schema({
+    brand:String,
+    model:String,
+    year:Number,
+    avail:Boolean
 });
 
-// async function run(){
-//     try{
-//         await client.connect();
-//         console.log('connected')
-//     } finally {
-//         await client.close();
-//     }
-// }
-// run().catch(console.dir);
-
-
-
-app.get('/api/users',async(req,res)=>{
-    try{
-        await client.connect();
-        const database = client.db('myApp');
-        const collection = database.collection('users');
-        const query = await collection.insertOne({
-            name:'Francis',
-            lastname:'Jones'
-        });
-        console.log(query);
-
-        res.status(200).json({awesome:'Yes'});
-    } catch(error){
-        throw error;
-    } finally{
-        await client.close();
-        console.log('all is done')
-    }
-});
+const Car = mongoose.model('Car',carSchema);
+///////////////////////////////
 
 
 
